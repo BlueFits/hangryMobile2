af24f29c6a26d77bb90544724e0d0d2093d22dee
jghori <59810056+jghori@users.noreply.github.com>
2020-11-04 22:26:27 -0500

    new categories and styling changes

---
+++ screens/Profile/profile.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Profile = () => {
+    return (
+        <View style={styles.fillerStyles}>
+            <Text>Will be implemented</Text>
+        </View>
+
+    );
+};
+
+const styles = StyleSheet.create({
+    fillerStyles: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
+
+export default Profile;
